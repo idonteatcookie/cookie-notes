4fb7a1700f77ccd1561a9eb7271647fc8f607a05
xieyiming03 <xieyiming03@meituan.com>
2025-03-23 17:51:26 +0800

    Deploy to gh-pages

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  // 确保你的应用适合静态部署
+  trailingSlash: true,  // 添加这行使URL更兼容静态托管
 };
 
 module.exports = nextConfig; 
